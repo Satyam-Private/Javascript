d9a9bdeeec8e13397c46a1ec6b0fef1b9f034a1f
Satyam-Private <satyampatil1252@gmail.com>
2025-08-10 06:31:13 +0530

    10/8/25

---
+++ DOM/Dynamic_manipulation/index.js
@@ -0,0 +1,15 @@
+let div = document.querySelector("div"); 
+
+let h2 = document.createElement("h2"); 
+h2.textContent = "hii i am fine"; 
+
+div.append(h2);  // append means you are adding at last 
+
+// now lets add it first 
+
+let h3 = document.createElement("h3"); 
+h3.textContent = "hiii this is prepended later to document "; 
+
+document.querySelector("div").prepend(h3)
+
+
