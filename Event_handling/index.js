fc6938ec16190760d107df75647fa7389571dec2
Satyam-Private <satyampatil1252@gmail.com>
2025-08-11 10:08:54 +0530

    11/8/25

---
+++ Event_handling/index.js
@@ -0,0 +1,22 @@
+let h1 = document.querySelector("h1"); 
+
+
+h1.addEventListener("click" , function(){ 
+    h1.textContent = "look my color is changed"; 
+    h1.style.color = "red";
+
+})
+
+let p = document.querySelector("p"); 
+console.dir(p);
+
+p.addEventListener("dblclick", function(){ 
+    p.style.fontStyle = "italic";
+
+}); 
+
+let input = document.querySelector(".my-input"); 
+
+input.addEventListener("input" , function(event_data){ 
+    alert("you typed " + event_data.data);
+})
